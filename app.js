ffbbe4105710e275594a3b1b897c33626748ba0f
nirajankarki5 <nirajankarki5@gmail.com>
2021-10-08 15:54:41 -0700

    express middleware practice

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("Hello world");
+app.use('/example', (req, res, next)=>{
+    console.log("In the middleware");
+    res.send("Example route");
+    // next();
+});
+
+app.use('/', (req, res, next)=>{
+    console.log("In another middleware");
+    res.send("Main route");
 });
 
 app.listen(3000);
