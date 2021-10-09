f9eaf9614fdbaa01c9f96c1a1a06d9c4768e56b9
nirajankarki5 <nirajankarki5@gmail.com>
2021-10-09 09:34:39 -0700

    added comments

--- routes/book.js
+++ routes/book.js
@@ -6,10 +6,13 @@ const bodyParser = require('body-parser');
 route.use('/public', express.static(path.join(__dirname, '../', 'views')));
 route.use(bodyParser.urlencoded({extended: false}));
 
+// in homepage, we display bookname.html file
 route.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '../', 'views', 'bookname.html'));
 });
 
+// once user submits, we display displaybook.html
+// routs is given in html form action="/display"
 route.post('/display', (req,res)=>{
     console.log(req.body);
 
