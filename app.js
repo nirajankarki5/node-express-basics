f2b4fc6254137719410f022db603e26fd061c36c
nirajankarki5 <nirajankarki5@gmail.com>
2021-10-09 17:05:28 -0700

    ejs template example

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-// a simple project that lets you enter books in homepage and redirect to /display page on button click
-
 const express = require('express');
 const app = express();
 
-const book = require('./routes/book');
+app.set('view engine', 'ejs');
+app.get('/:userQuery', (req,res)=>{
+    res.render('index', {data:{userQuery: req.params.userQuery,
+                            books: ["Book 1", "Book 2", "Book 3", "Book 4"]}});
+});
 
-app.use('/', book);
-
-app.listen(3000);
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000');
+});
